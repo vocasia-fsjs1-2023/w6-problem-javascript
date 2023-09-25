a01e6ceb4e31bb4594b58db2ca6b745778e526ff
FadilahSuryani <suryanifadilah411@gmail.com>
2023-09-25 14:33:00 +0700

    Tugas-Week6

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,30 @@
-let num = 3
+// let num = 3;
 
 // Tulis code dibawah sini.
-// dan console log hasil
+function createTriangle(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+
+    // Menambahkan spasi sebelum karakter "x"
+    for (let j = 1; j <= num - i; j++) {
+      row += " ";
+    }
+
+    // Menambahkan karakter "x"
+    for (let j = 1; j <= 2 * i - 1; j++) {
+      if (j % 2 === 1) {
+        row += "x";
+      } else {
+        row += "o";
+      }
+    }
+
+    console.log(row);
+  }
+}
+
+let num = 3;
+createTriangle(num);
+
+num = 6;
+createTriangle(num);
--- problem-7/index.js
+++ problem-7/index.js
@@ -1,70 +1,86 @@
-function highestScore (students) {
-    // Code disini
+function highestScore(students) {
+  // Code disini
+  var result = {};
+
+  for (var i = 0; i < students.length; i++) {
+    var student = students[i];
+
+    if (!result[student.class] || student.score > result[student.class].score) {
+      result[student.class] = {
+        name: student.name,
+        score: student.score,
+      };
+    }
   }
-  
-  // TEST CASE
-  console.log(highestScore([
+
+  return result;
+}
+
+// TEST CASE
+console.log(
+  highestScore([
     {
-      name: 'Dimitri',
+      name: "Dimitri",
       score: 90,
-      class: 'foxes'
+      class: "foxes",
     },
     {
-      name: 'Alexei',
+      name: "Alexei",
       score: 85,
-      class: 'wolves'
+      class: "wolves",
     },
     {
-      name: 'Sergei',
+      name: "Sergei",
       score: 74,
-      class: 'foxes'
+      class: "foxes",
     },
     {
-      name: 'Anastasia',
+      name: "Anastasia",
       score: 78,
-      class: 'wolves'
-    }
-  ]));
-  
-  // {
-  //   foxes: { name: 'Dimitri', score: 90 },
-  //   wolves: { name: 'Alexei', score: 85 }
-  // }
-  
-  
-  console.log(highestScore([
+      class: "wolves",
+    },
+  ])
+);
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+console.log(
+  highestScore([
     {
-      name: 'Alexander',
+      name: "Alexander",
       score: 100,
-      class: 'foxes'
+      class: "foxes",
     },
     {
-      name: 'Alisa',
+      name: "Alisa",
       score: 76,
-      class: 'wolves'
+      class: "wolves",
     },
     {
-      name: 'Vladimir',
+      name: "Vladimir",
       score: 92,
-      class: 'foxes'
+      class: "foxes",
     },
     {
-      name: 'Albert',
+      name: "Albert",
       score: 71,
-      class: 'wolves'
+      class: "wolves",
     },
     {
-      name: 'Viktor',
+      name: "Viktor",
       score: 80,
-      class: 'tigers'
-    }
-  ]));
-  
-  // {
-  //   foxes: { name: 'Alexander', score: 100 },
-  //   wolves: { name: 'Alisa', score: 76 },
-  //   tigers: { name: 'Viktor', score: 80 }
-  // }
-  
-  
-  console.log(highestScore([])); //{}
+      class: "tigers",
+    },
+  ])
+);
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+console.log(highestScore([])); //{}
