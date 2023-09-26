eaadb54104aaf6083357bf487045bca7c3bf47a0
rosanhusen <rosanhusen305@gmail.com>
2023-09-26 19:20:27 +0700

    pengumpulan tugas w6-javascript

--- problem-2/index.js
+++ problem-2/index.js
@@ -1,5 +1,14 @@
 function ladder(word) {
-//your code here
+    const ladderArray = [];
+    const wordArray = word.split('');
+    for (let i = wordArray.length; i > 0; i--) {
+        const final = [];
+        for (let j = 0; j < i; j++) {
+            final.push(wordArray[j]);
+        }
+        ladderArray.push(final);
+    }
+    return ladderArray;
 }
   
 console.log(ladder('vocasia'));
