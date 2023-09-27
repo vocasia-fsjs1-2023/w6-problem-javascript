f3533af20aaeee9c8f52d441d8b6574ebe85df4a
juasruhiyat15 <juasruhiyat567@gmail.com>
2023-09-27 08:37:19 +0800

    Finish Problem 3

--- problem-3/index.js
+++ problem-3/index.js
@@ -1,4 +1,22 @@
-let num = 3
+let num = 6;
+let text = '';
 
 // Tulis code dibawah sini.
+for (let i = 0; i < num; i++) {
+    let box = ' ';
+
+    for (let j = 0; j < num - i - 1; j++) {
+        box += ' ';
+
+    }
+
+    for (let k = 0; k < (i * 2) + 1; k++){
+        if(k % 2 ==0 ){
+            box += 'x';
+        }else{
+            box += 'o';
+        }
+    }
+    console.log(box);
+}
 // dan console log hasil
