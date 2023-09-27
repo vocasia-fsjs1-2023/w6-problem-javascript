31a72f1e9d36892e7038b82e2b6893ba05c28344
juasruhiyat15 <juasruhiyat567@gmail.com>
2023-09-27 08:40:01 +0800

    Finish Problem 8

--- problem-8/index.js
+++ problem-8/index.js
@@ -1,5 +1,12 @@
 function cariMedian(arr) {
     // code disini
+    arr.sort((a, b) => a - b);
+    if (arr.length % 2 === 1) {
+        return arr[Math.floor(arr.length / 2)];
+    } else {
+        const midIndex = arr.length / 2;
+        return (arr[midIndex - 1] + arr[midIndex]) / 2;
+    }
   }
   
   // TEST CASES
