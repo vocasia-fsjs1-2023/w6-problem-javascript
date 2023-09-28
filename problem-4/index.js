37e49659a0ee07f8156063e0c92bd78eef407416
evaald <evaaldori239@gmail.com>
2023-09-28 08:47:48 +0700

    Problem 4 Eva

--- problem-4/index.js
+++ problem-4/index.js
@@ -1,17 +1,26 @@
 function changeMe(arr) {
-    // Tulis codemu disini dan console log hasilnya
+  let currentYear = new Date().getFullYear(); 
+
+  for (let i = 0; i < arr.length; i++) {
+    let personData = {
+      firstName: arr[i][0],
+      lastName: arr[i][1],
+      gender: arr[i][2],
+      age: 'Invalid Birth Year' 
+    };
+
+
+    if (arr[i][3] !== undefined && arr[i][3] <= currentYear) {
+d
+      personData.age = currentYear - arr[i][3];
+    }
+
+
+    console.log(i + 1 + '. ' + personData.firstName + ' ' + personData.lastName + ':');
+    console.log(personData);
+  }
 }
-  
-  // TEST CASES
-  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
-  // 1. Christ Evans:
-  // { firstName: 'Christ',
-  //   lastName: 'Evans',
-  //   gender: 'Male',
-  //   age: 38 }
-  // 2. Robert Downey:
-  // { firstName: 'Robert',
-  //   lastName: 'Downey',
-  //   gender: 'Male',
-  //   age: 'Invalid Birth Year' }
-  changeMe([]); // ""
+
+
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]);
+changeMe([]);
