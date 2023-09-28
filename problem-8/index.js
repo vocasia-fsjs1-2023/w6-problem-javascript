7192804db0a6552e550ea16ca7fe7a72835b5f7c
evaald <evaaldori239@gmail.com>
2023-09-28 08:52:05 +0700

    Problem 8 Eva

--- problem-8/index.js
+++ problem-8/index.js
@@ -1,10 +1,28 @@
 function cariMedian(arr) {
-    // code disini
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
   }
-  
-  // TEST CASES
-  console.log(cariMedian([1, 2, 3, 4, 5])); // 3
-  console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
-  console.log(cariMedian([3, 4, 7, 6, 10])); // 7
-  console.log(cariMedian([1, 3, 3])); // 3
-  console.log(cariMedian([7, 7, 8, 8])); // 7.5
+
+
+  const middleIndex = Math.floor(arr.length / 2);
+
+
+  if (arr.length % 2 === 1) {
+    return arr[middleIndex];
+  } else {
+    const middleValues = (arr[middleIndex - 1] + arr[middleIndex]) / 2;
+    return middleValues;
+  }
+}
+
+console.log(cariMedian([1, 2, 3, 4, 5])); // 3
+console.log(cariMedian([1, 3, 4, 10, 12, 13])); // 7
+console.log(cariMedian([3, 4, 7, 6, 10])); // 7
+console.log(cariMedian([1, 3, 3])); // 3
+console.log(cariMedian([7, 7, 8, 8])); // 7.5
