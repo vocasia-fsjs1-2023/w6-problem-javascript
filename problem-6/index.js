369283716e9d9e4e9fb41aacadf29205cd7fc0b2
johan <johan992001@gmail.com>
2023-10-02 23:05:33 +0800

    pengumpulan tugas minggu 6 JavaScript

--- problem-6/index.js
+++ problem-6/index.js
@@ -1,45 +1,56 @@
 function deepSum (arr) {
-    // Code disini
+  // Code disini
+let tambah = 0;
+for (let i = 0; i < arr.length; i++) {
+  let luarArr = arr[i];
+  for (let j = 0; j < luarArr.length; j++) {
+    let dalamArr = luarArr[j];
+    for (let k = 0; k < dalamArr.length; k++) {
+        tambah += dalamArr[k];
+    }
   }
-  
-  //TEST CASE
-  console.log(deepSum([
-    [
-      [4, 5, 6],
-      [9, 1, 2, 10],
-      [9, 4, 3]
-    ],
-    [
-      [4, 14, 31],
-      [9, 10, 18, 12, 20],
-      [1, 4, 90]
-    ],
-    [
-      [2, 5, 10],
-      [3, 4, 5],
-      [2, 4, 5, 10]
-    ]
-  ])); // 316
-  
-  console.log(deepSum([
-    [
-      [20, 10],
-      [15],
-      [1, 1]
-    ],
-    [
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-      [2],
-      [9, 11]
-    ],
-    [
-      [3, 5, 1],
-      [1, 5, 3],
-      [1]
-    ],
-    [
-      [2]
-    ]
-  ])); // 156
-  
-  console.log(deepSum([])); // No number
+}
+return tambah;
+}
+
+//TEST CASE
+console.log(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])); // 316
+
+console.log(deepSum([
+  [
+    [20, 10],
+    [15],
+    [1, 1]
+  ],
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [2],
+    [9, 11]
+  ],
+  [
+    [3, 5, 1],
+    [1, 5, 3],
+    [1]
+  ],
+  [
+    [2]
+  ]
+])); // 156
+
+console.log(deepSum([])); // No number
